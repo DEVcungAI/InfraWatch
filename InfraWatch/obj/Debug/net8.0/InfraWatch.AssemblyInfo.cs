b11e703c67100df377910932164dcebc9790b1ce
DEVcungAI <167099435+DEVcungAI@users.noreply.github.com>
2026-06-15 21:07:10 +0700

    Rebuild project and update files.
    Rebuilt project artifacts and updated generated metadata. AssemblyInformationalVersion and SourceLink were bumped to the new build commit (updated to 9f46a2fd...), .vs DocumentLayout view state changed, and multiple bin/obj files and build cache files were refreshed as part of the build.

--- InfraWatch/obj/Debug/net8.0/InfraWatch.AssemblyInfo.cs
+++ InfraWatch/obj/Debug/net8.0/InfraWatch.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InfraWatch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5266af06e3a55af04832d9559635fc6f9b0fea3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f46a2fdc1abdfe5a6565345bf56a3fe887df9ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("InfraWatch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InfraWatch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
